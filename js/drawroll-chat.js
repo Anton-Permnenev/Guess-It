6b3e2c086795fc14fdf66faa15b9cc8ae3f4abbc
Vasiliy <vurosov@gmail.com>
2014-11-15 18:40:40 +0300

    Chat added to drawroll

---
+++ js/drawroll-chat.js
@@ -0,0 +1,27 @@
+var chat_channel = 'drawroll-chat';
+
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-ccca9b35-6dce-48e8-8b92-a19ba6376d04',
+    subscribe_key: 'sub-c-673b9e80-6787-11e4-814d-02ee2ddab7fe'
+});
+
+pubnub.subscribe({
+    channel: chat_channel,
+    callback: insertIntoChat
+});
+
+function insertIntoChat(message) {
+    var code = '<p><b>' + message.author + ':</b> ' + message.answer + '</p>';
+    $("#chat").find("#fence").append(code);
+};
+
+document.getElementById('submit').addEventListener('click', function() {
+    pubnub.publish({
+        channel: chat_channel,
+        message: {
+            author: document.getElementById('author').value,
+            answer: document.getElementById('answer').value
+        }
+    });
+});
+
