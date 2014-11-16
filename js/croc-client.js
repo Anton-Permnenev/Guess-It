e51757c4dc5900640861255ef5a8918f09b3bd70
Vasiliy <vurosov@gmail.com>
2014-11-16 06:48:13 +0300

    Test

--- js/croc-client.js
+++ js/croc-client.js
@@ -56,3 +56,37 @@ $("#answer").keyup(function (event) {
         publishAttempt();
     }
 });
+
+// Let's make a call
+(function(){
+    // ~Warning~ You must get your own API Keys for non-demo purposes.
+    // ~Warning~ Get your PubNub API Keys: http://www.pubnub.com/get-started/
+    // The phone *number* can by any string value
+    var phone = PHONE({
+        number        : (1000 + (1 + Math.random()*1000)^0).toString(), // Random number to the client
+        publish_key   : 'pub-c-ccca9b35-6dce-48e8-8b92-a19ba6376d04',
+        subscribe_key : 'sub-c-673b9e80-6787-11e4-814d-02ee2ddab7fe',
+        ssl           : true
+    });
+
+    // As soon as the phone is ready we can make calls
+    phone.ready(function(){
+
+        // Dial a Number and get the Call Session
+        // For simplicity the phone number is the same for both caller/receiver.
+        // you should use different phone numbers for each user.
+        var session = phone.dial(num);
+
+    });
+
+    // When Call Comes In or is to be Connected
+    phone.receive(function(session){
+
+        // Display Your Friend's Live Video
+        session.connected(function(session){
+            PUBNUB.$('video-out').appendChild(session.video);
+        });
+
+    });
+
+})();
