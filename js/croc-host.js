af5c403dc2d9c50bc87099dddc0668682dd6a014
Vasiliy <vurosov@gmail.com>
2014-11-16 04:29:47 +0300

    Prepared croc-host

--- js/croc-host.js
+++ js/croc-host.js
@@ -1,35 +1,64 @@
-var chat_channel = 'drawroll-chat';
+var num = '-1'; // пригодится
+(function () {
+    var rand = (1 + Math.random() * (1000)) ^ 0;
+    num = rand.toString();
+    var passphrase;
+    var phone = PHONE({
+        number: num,
+        publish_key: 'pub-c-ccca9b35-6dce-48e8-8b92-a19ba6376d04',
+        subscribe_key: 'sub-c-673b9e80-6787-11e4-814d-02ee2ddab7fe',
+        ssl: true
+    });
+    document.getElementById('num').innerHTML = num;
+
+    $("#initModal").modal({
+        show: true
+    });
+
+    // As soon as the phone is ready we can make calls
+    phone.ready(function () {
+
+        PUBNUB.bind('mousedown,touchstart', PUBNUB.$('tryguess'), function () {
+            passphrase = PUBNUB.$('word').value;
+            document.getElementById('ww').innerHTML = passphrase;
+            document.getElementById('word').value = "";
+            $("#initModal").modal('hide');
+        });
+        $("#word").keyup(function(event) {
+            if (event.keyCode === 13) {
+                passphrase = document.getElementById('word').value;
+                document.getElementById('ww').innerHTML = passphrase;
+                document.getElementById('word').value = "";
+                $("#initModal").modal('hide');
+            }
+        });
+    });
+
+
+})();
+
+var chat_channel = 'croc-chat' + num;
 var pubnub = PUBNUB.init({
     publish_key: 'pub-c-ccca9b35-6dce-48e8-8b92-a19ba6376d04',
     subscribe_key: 'sub-c-673b9e80-6787-11e4-814d-02ee2ddab7fe'
 });
-var UUID = PUBNUB.db.get('session') || (function(){
-    var uuid = PUBNUB.uuid();
-    PUBNUB.db.set( 'session',uuid );
-    return uuid;
-})();
 
 pubnub.subscribe({
     channel: chat_channel,
-    callback: insertIntoChat
+    callback: responseToChat
 });
 
-function insertIntoChat(message) {
+function responseToChat(message) {
     var dist = message.UUID + "^" + message.author + "^" + message.answer;
     var crypto = CryptoJS.MD5(dist);
 
     if (message.type === 'attempt') {
-        var code = '<p>';
-
-        // host only
-        if ($("#fence").data("param") === 'host') {
-            code += "<div class=\"col-sm-4\" id=\""+ crypto + "_controls\">" +
+        var code = '<p>' +
+                    "<div class=\"col-sm-4\" id=\""+ crypto + "_controls\">" +
                     "<button onclick='confirmAttempt(\"" + dist + '\")' + "'><i class='glyphicon glyphicon-thumbs-up'></i></button>" +
                     "<button onclick='rejectAttempt(\"" + dist + '\")' + "'><i class='glyphicon glyphicon-thumbs-down'></i></button>" +
                     "</div>";
-        }
-
-        code += '<div id="' + crypto + '" class="col-sm-8"><b>' + message.author + ':</b> ' + message.answer + '</div></p>';
+        code +=     '<div id="' + crypto + '" class="col-sm-8"><b>' + message.author + ':</b> ' + message.answer + '</div></p>';
 
         $("#chat").find("#fence").prepend(code);
     } else if (message.type === 'err') {
@@ -65,27 +94,4 @@ var rejectAttempt = function(dist) {
         }
     });
     $("#" + CryptoJS.MD5(dist) + "_controls").remove();
-}
-
-// client-only
-var publishAttempt = function() {
-    pubnub.publish({
-        channel: chat_channel,
-        message: {
-            UUID: UUID,
-            author: document.getElementById('author').value,
-            answer: document.getElementById('answer').value,
-            type: 'attempt'
-        }
-    });
-    document.getElementById('answer').value = "";
-}
-
-if ($("#fence").data("param") === 'client') {
-    document.getElementById('submit').addEventListener('click', publishAttempt);
-    $("#answer").keyup(function (event) {
-        if (event.keyCode === 13) {
-            publishAttempt();
-        }
-    });
 }
